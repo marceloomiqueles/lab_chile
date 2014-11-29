6d89a940c93339cc8ed704c14f60892188c26e05
marceloomiqueles <marcelomiqueles@hotmail.com>
2014-11-29 10:05:23 -0300

    Primeros Pasos
    Se crea el index con una hoja de estilos

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>LabChile</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+	<body>
+		<div id='bigBox'>
+			<div id='menuHead'>
+				<div id='menuInterior'></div>
+			</div>
+		</div>
+	</body>
+</html>
